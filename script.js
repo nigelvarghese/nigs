12d1541cf1cb6bff55e24fd166c69a92973f96f8
unknown <ni4nigman@yahoo.com>
2020-06-10 10:25:52 +0300

    final

---
+++ script.js
@@ -0,0 +1,68 @@
+function show(col){
+    document.getElementById("displayHead").style.background=col
+}
+
+function pressN(val){
+  document.getElementById("numeral").value=val
+}
+
+function pressA(val){
+  document.getElementById("alphabet").value=val
+}
+
+
+function pic(imgval){
+    if(imgval==1){
+        document.getElementById("imge").src ="images/alpha_image_A.png";
+    }else if(imgval==2){
+        document.getElementById("imge").src ="images/alpha_image_B.png";
+    }else if(imgval==3){
+        document.getElementById("imge").src ="images/alpha_image_C.png";
+    }else if(imgval==4){
+        document.getElementById("imge").src ="images/alpha_image_D.png";
+    }else if(imgval==5){
+        document.getElementById("imge").src ="images/alpha_image_E.png";
+    }else if(imgval==6){
+        document.getElementById("imge").src ="images/alpha_image_F.png";
+    }else if(imgval==7){
+        document.getElementById("imge").src ="images/alpha_image_G.png";
+    }else if(imgval==8){
+        document.getElementById("imge").src ="images/alpha_image_H.png";
+    }else if(imgval==9){
+        document.getElementById("imge").src ="images/alpha_image_I.png";
+    }else if(imgval==10){
+        document.getElementById("imge").src ="images/alpha_image_J.png";
+    }else if(imgval==11){
+        document.getElementById("imge").src ="images/alpha_image_K.png";
+    }else if(imgval==12){
+        document.getElementById("imge").src ="images/alpha_image_L.png";
+    }else if(imgval==13){
+        document.getElementById("imge").src ="images/alpha_image_M.png";
+    }else if(imgval==14){
+        document.getElementById("imge").src ="images/alpha_image_N.png";
+    }else if(imgval==15){
+        document.getElementById("imge").src ="images/alpha_image_O.png";
+    }else if(imgval==16){
+        document.getElementById("imge").src ="images/alpha_image_P.png";
+    }else if(imgval==17){
+        document.getElementById("imge").src ="images/alpha_image_Q.png";
+    }else if(imgval==18){
+        document.getElementById("imge").src ="images/alpha_image_R.png";
+    }else if(imgval==19){
+        document.getElementById("imge").src ="images/alpha_image_S.png";
+    }else if(imgval==20){
+        document.getElementById("imge").src ="images/alpha_image_T.png";
+    }else if(imgval==21){
+        document.getElementById("imge").src ="images/alpha_image_U.png";
+    }else if(imgval==22){
+        document.getElementById("imge").src ="images/alpha_image_V.png";
+    }else if(imgval==23){
+        document.getElementById("imge").src ="images/alpha_image_W.png";
+    }else if(imgval==24){
+        document.getElementById("imge").src ="images/alpha_image_X.png";
+    }else if(imgval==25){
+        document.getElementById("imge").src ="images/alpha_image_Y.png";
+    }else if(imgval==26){
+        document.getElementById("imge").src ="images/alpha_image_Z.png";
+    }
+}
